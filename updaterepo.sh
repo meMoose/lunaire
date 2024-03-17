db4976ec9958fef543c7254d2c71e8d53ef93371
memoose <gathering011@gmail.com>
2024-03-17 21:34:06 +0800

    init

--- updaterepo.sh
+++ updaterepo.sh
@@ -4,7 +4,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then # Linux usage of repo.me
     
     rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release 2> /dev/null
     
-    apt-ftparchive packages ./debians > Packages
+    apt-ftparchive packages ./debs > Packages
     gzip -c9 Packages > Packages.gz
     xz -c9 Packages > Packages.xz
     zstd -c19 Packages > Packages.zst
@@ -31,7 +31,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then # Linux usage of repo.me
     
     rm {Packages{,.xz,.gz,.bz2,.zst},Release{,.gpg}} 2> /dev/null
     
-    ./apt-ftparchive packages ./debians > Packages
+    ./apt-ftparchive packages ./debs > Packages
     gzip -c9 Packages > Packages.gz
     xz -c9 Packages > Packages.xz
     zstd -c19 Packages > Packages.zst
@@ -45,7 +45,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then # Linux usage of repo.me
     
     rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release 2> /dev/null
     
-    apt-ftparchive packages ./debians > Packages
+    apt-ftparchive packages ./debs > Packages
     gzip -c9 Packages > Packages.gz
     xz -c9 Packages > Packages.xz
     zstd -c19 Packages > Packages.zst
@@ -59,7 +59,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then # Linux usage of repo.me
     
     rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release 2> /dev/null
     
-    apt-ftparchive packages ./debians > Packages
+    apt-ftparchive packages ./debs > Packages
     gzip -c9 Packages > Packages.gz
     xz -c9 Packages > Packages.xz
     zstd -c19 Packages > Packages.zst
@@ -77,7 +77,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then # Linux usage of repo.me
 
     rm {Packages{,.xz,.gz,.bz2,.zst},Release{,.gpg}} 2> /dev/null
 
-    apt-ftparchive packages ./debians > Packages
+    apt-ftparchive packages ./debs > Packages
     gzip -c9 Packages > Packages.gz
     xz -c9 Packages > Packages.xz
     zstd -c19 Packages > Packages.zst
